3e59b1ed43dfbfb2b1971d3055eb3da1dddedbac
vamsi jagatha <vamsijagatha31@gmail.com>
2024-12-31 19:14:25 +0530

    Enhance Card component: - Added CountUp for animated amount display. - Ensured amount validation. - Simplified JSX structure for clarity. - Improved UX with dynamic animation and reset on change.

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -1,19 +1,33 @@
+import CountUp from "react-countup";
+
 const Card = ({ amount, icon, title, bgColor, iconColor }) => {
+  // Ensure amount is a valid number
+  const isValidAmount = !isNaN(amount);
+
   return (
     <div
-      className={`flex flex-col items-center justify-center min-w-fit min-h-40 shadow-xl ${bgColor} p-4 md:p-6 lg:p-8 border rounded-3xl mb-6`}
+      className={`flex items-center justify-center shadow-xl ${bgColor} border rounded-3xl mb-6 p-4`}
     >
-      <p
-        className={`flex items-center text-lg md:text-xl lg:text-2xl font-semibold text-center`}
-      >
-        <span className={`mr-2 text-4xl md:text-5xl lg:text-6xl ${iconColor}`}>
-          {icon}
-        </span>
-        {title && <span className="text-white">{title}</span>}
-      </p>
-      <p className="text-2xl md:text-3xl lg:text-4xl font-bold text-white mt-2">
-        Rs.{amount}
-      </p>
+      <span className={`mr-2 text-3xl md:text-4xl lg:text-5xl ${iconColor}`}>
+        {icon}
+      </span>
+      <div className="text-xl md:text-xl lg:text-2xl font-bold text-white mt-2">
+        <div className={`text-lg md:text-xl lg:text-2xl font-semibold`}>
+          {title && <span className="text-white text-lg">{title}</span>}
+        </div>
+        Rs.
+        {isValidAmount && (
+          <CountUp
+            key={amount} // Ensure CountUp resets on amount change
+            start={0}
+            end={amount}
+            duration={2.5}
+            delay={0.5}
+            separator=","
+            useEasing={true}
+          />
+        )}
+      </div>
     </div>
   );
 };
